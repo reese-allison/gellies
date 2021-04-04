57bef64c2b65d67cb222a3458bf3bc43e1ade9d9
reese-allison <mrreeseallison@gmail.com>
2021-04-04 15:54:10 -0400

    Add bouncing

--- static/js/moji.js
+++ static/js/moji.js
@@ -2,8 +2,6 @@ import { html, Component } from 'https://unpkg.com/htm/preact/index.mjs?module'
 import { gsap, TimelineMax } from 'https://unpkg.com/gsap?module';
 import { randomRange, randomNegative } from './common.js';
 
-const ebb_tl = new TimelineMax({repeat:-1, repeatDelay:.5, yoyo:true});
-
 
 function eye_movement(inner_eyes){
     let eye_movement_delay = randomRange(0, 1);
@@ -24,8 +22,10 @@ class Moji extends Component {
     constructor(props){
         super(props);
 
-        this.chewing = false;
+        this.active = false;
         this.chew = this.chew.bind(this);
+        this.bounce = this.bounce.bind(this);
+        this.ebb = new TimelineMax({repeat:-1, repeatDelay:.5, yoyo:true});
     }
 
     componentDidMount(){
@@ -39,7 +39,6 @@ class Moji extends Component {
     }
 
     componentWillUnmount(){
-        ebb_tl.remove(this.ebb);
         this.eye_movement.kill();
     }
 
@@ -48,39 +47,93 @@ class Moji extends Component {
         this.eyes = document.getElementById(`moji-${ this.props.id }-eyes`);
         this.mouth = document.getElementById(`moji-${ this.props.id }-mouth`);
         this.cheeks = document.getElementById(`moji-${ this.props.id }-cheeks`);
-        this.ebb = gsap.from(this.moji, {
+        this.ebb.add(gsap.from(this.moji, {
             scale: 1.03,
             duration: 2,
             ease: "linear",
             transformOrigin: "top"
-        });
-        ebb_tl.add(this.ebb, randomRange(0, 2));
+        }), randomRange(0, 2));
 
         let inner_eyes = document.querySelectorAll(`#moji-${ this.props.id } .inner-eye`);
         this.eye_movement = gsap.set(eye_movement, {onRepeat: eye_movement, onRepeatParams: [inner_eyes], repeat: -1, repeatDelay: 2});
 
-        // Test chewing on click
+        // // Test chewing on click
+        // this.moji.addEventListener("click", e => {
+        //     if(!this.active){
+        //         this.chew().play();
+        //     }
+        // });
+
+        // // Test bounce on click
+        // this.moji.addEventListener("click", e => {
+        //     if(!this.active){
+        //         this.bounce().play();
+        //     }
+        // });
+
+        // Test jump on click
         this.moji.addEventListener("click", e => {
-            if(!this.chewing){
-                this.chew();
+            if(!this.active){
+                this.jump().play();
             }
         });
     }
 
     chew(){
-        this.chewing = true;
-        let chew_tl = new TimelineMax({repeat: 4, yoyo:true, onComplete: () => {
+        this.active = true;
+        let chew_tl = new TimelineMax({paused: true, repeat: 4, yoyo:true, onComplete: () => {
             gsap.to(this.mouth, {y: 0, duration: 1})
             gsap.fromTo(this.mouth, {scale: 0.85, transformOrigin: "center"}, {scale: 1, duration:1, transformOrigin: "center"}, 0)
             gsap.to(this.cheeks, {y: 0, duration: 1, onComplete: () => {
                 this.eyes.style.clipPath = null
-                this.chewing = false;
+                this.active = false;
             }})
         }});
         this.eyes.style.clipPath = `url(#moji-${ this.props.id }-cheeks)`;
         chew_tl.to(this.mouth, {y: -5}, 0)
         chew_tl.to(this.mouth, {scale: 0.85, transformOrigin: "center"}, 0)
         chew_tl.to(this.cheeks, {y: -30}, 0);
+
+        return chew_tl;
+    }
+
+    open_mouth(){
+
+    }
+
+    bounce(){
+        this.active = true;
+        this.ebb.pause();
+        let bounce_tl = new TimelineMax({paused: true, onReverseComplete: ()=>{
+            this.active = false;
+            this.ebb.resume();
+        }});
+
+        bounce_tl.to(this.moji, .4, {
+            transformOrigin: "50% 100%",
+            scaleX: 1.1,
+            scaleY: 0.8,
+            ease: "power1.inOut",
+            onComplete: () => {
+                bounce_tl.reverse();
+            }
+        })
+
+        return bounce_tl;
+    }
+
+    // TODO don't let child animation calls resume ebbing prematurely if part of an animation group
+    jump(){
+        this.active = true;
+        this.ebb.pause();
+        let jump_tl = new TimelineMax({paused: true, onComplete: ()=>{
+            this.active = false;
+            this.ebb.resume();
+        }});
+
+        jump_tl.add(this.bounce().play());
+
+        return jump_tl;
     }
 
     render(){
@@ -90,4 +143,4 @@ class Moji extends Component {
     }
 }
 
-export{ Moji }
+export { Moji }
