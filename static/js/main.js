78bb15352f810d13e1b44d3757df1d8fd10fdca9
gumphso <60444720+gumphso@users.noreply.github.com>
2021-08-07 23:15:21 -0400

    Color Picker Class

--- static/js/main.js
+++ static/js/main.js
@@ -4,6 +4,38 @@ import { Moji } from './moji.js';
 
 gsap.ticker.fps(30);
 
+class Gradient_Moji extends Moji{
+    constructor(props){
+        super(props);
+    }
+
+    componentDidUpdate(){
+        if (!this.state.setup){
+            this.moji = document.getElementById(`moji-${ this.state.id }`);
+            this.eyes = document.getElementById(`moji-${ this.state.id }-eyes`);
+            this.mouth = document.getElementById(`moji-${ this.state.id }-mouth`);
+            this.cheeks = document.getElementById(`moji-${ this.state.id }-cheeks`);
+            this.shadow = document.getElementById(`moji-${ this.state.id }-shadow`);
+            this.setState({setup: true});
+
+            ['click','ontouchstart'].forEach( evt => {
+                this.moji.addEventListener(evt, e => {
+                    let gradient_name = prompt('Gradient Name');
+                    fetch('/save/', {
+                       method: 'post',
+                        body: JSON.stringify({
+                           'name': gradient_name.toLowerCase(),
+                            'html' : document.getElementById(`moji-${ this.state.id }-body-color`).outerHTML
+                        })
+                    })
+                });
+            });
+        }
+    }
+}
+
+
+
 var main = document.getElementById('main');
 
-render(Array(500).fill(0).map(_ => html`<${Moji}/>`), main);
+render(Array(100).fill(0).map(_ => html`<${Gradient_Moji}/>`), main);
