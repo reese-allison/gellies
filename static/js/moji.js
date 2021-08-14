8aab965172c70978e256f4636ef1bee3432f5cf7
reese-allison <mrreeseallison@gmail.com>
2021-08-14 15:11:31 -0400

    Fix the templating for object rendering and add JS to object

--- static/js/moji.js
+++ static/js/moji.js
@@ -1,6 +1,11 @@
-import { html, Component } from 'https://unpkg.com/htm/preact/index.mjs?module'
-import { gsap, TimelineMax } from 'https://unpkg.com/gsap?module';
-import { randomRange, randomNegative } from './common.js';
+function randomRange(min, max) { 
+    return Math.random() * (max - min) + min;
+}
+
+
+function randomNegative() {
+    return Math.random() < 0.5 ? -1 : 1; 
+}
 
 
 function eye_movement(inner_eyes){
@@ -18,184 +23,117 @@ function eye_movement(inner_eyes){
 }
 
 
-class Moji extends Component {
-    constructor(props){
-        super(props);
+class Moji {
+    constructor(id){
+        this.id = id
+        this.busy = false;
 
-        this.state = {
-            id: this.props.id ? this.props.id : 0,
-            setup: false,
-            states: ['chewing', 'bouncing', 'jumping'],
-            active: []
-        };
+        this.moji = document.getElementById(`moji-${ this.id }`);
+        this.shadow = document.getElementById(`moji-${ this.id }-shadow`);
 
-        this.active = this.active.bind(this);
-        this.inactive = this.inactive.bind(this);
-
-        this.bounce = this.bounce.bind(this);
-        this.jump = this.jump.bind(this);
-        this.busy = this.busy.bind(this);
+        let inner_eyes = document.querySelectorAll(`#moji-${ this.id } .inner-eye`);
+        this.eye_movement = gsap.set(eye_movement, {onRepeat: eye_movement, onRepeatParams: [inner_eyes], repeat: -1, repeatDelay: 2});
 
         this.ebb = new TimelineMax({repeat:-1, repeatDelay:.5, yoyo:true});
-    }
+        this.ebb.add(gsap.from(this.moji, {
+            scale: 1.03,
+            duration: 2,
+            ease: "linear",
+            transformOrigin: "top"
+        }), randomRange(0, 2));
 
-    componentDidMount(){
-        fetch(`/moji/${this.state.id ? this.state.id : 0}`, {method: 'GET'})
-        .then(function(response){
-            return response.json();
+        this.moji.addEventListener('click', e => {
+            if(Math.random() > .25){
+                this.bounce();
+            }
+            else{
+                this.jump();
+            }
         })
-        .then(function(json){
-            this.setState({
-                id: json.id,
-                moji: json.html
-            });
-        }
-        .bind(this));
-    }
-
-    componentWillUnmount(){
-        this.eye_movement.kill();
-    }
-
-    componentDidUpdate(){
-        if (!this.state.setup){
-            this.moji = document.getElementById(`moji-${ this.state.id }`);
-            this.eyes = document.getElementById(`moji-${ this.state.id }-eyes`);
-            this.mouth = document.getElementById(`moji-${ this.state.id }-mouth`);
-            this.cheeks = document.getElementById(`moji-${ this.state.id }-cheeks`);
-            this.shadow = document.getElementById(`moji-${ this.state.id }-shadow`);
-            this.ebb.add(gsap.from(this.moji, {
-                scale: 1.03,
-                duration: 2,
-                ease: "linear",
-                transformOrigin: "top"
-            }), randomRange(0, 2));
-    
-            let inner_eyes = document.querySelectorAll(`#moji-${ this.state.id } .inner-eye`);
-            this.eye_movement = gsap.set(eye_movement, {onRepeat: eye_movement, onRepeatParams: [inner_eyes], repeat: -1, repeatDelay: 2});
-            this.setState({setup: true});
-
-            ['click','ontouchstart'].forEach( evt => {
-                this.moji.addEventListener(evt, e => {
-                    if(Math.random() > .25){
-                        if(!this.busy()){
-                            this.bounce();
-                        }
-                    }
-                    else{
-                        if(!this.busy()){
-                            this.jump();
-                        }
-                    }
-                });
-            });
-        }
-    }
-
-    active(state, callback){
-        if (!this.state.active.includes(state)){
-            this.setState({active: [...this.state.active, state]}, ()=>{
-                this.ebb.pause();
-                if (callback){
-                    callback();
-                }
-            });
-        }
-    }
-
-    inactive(state, callback){
-        if (this.state.active.includes(state)){
-            this.setState({active: this.state.active.filter(item => item !== state)}, ()=>{
-                if(this.state.active.length === 0){
-                    this.ebb.resume();
-                    if (callback){
-                        callback();
-                    }
-                }
-            });
-        }
     }
 
     bounce(){
-        this.active('bouncing', ()=>{
-            let bounce_tl = new TimelineMax({ onReverseComplete: ()=>{
-                this.inactive('bouncing');
-            }});
-    
-            bounce_tl.to(this.moji, .3, {
-                transformOrigin: "50% 100%",
-                scaleX: 1.1,
-                scaleY: 0.8,
-                ease: "power1.inOut",
-                onComplete: () => {
-                    bounce_tl.reverse();
-                }
-            })
-        });
+        if (this.busy){
+            return;
+        }
+        this.active();
+        let bounce_tl = new TimelineMax({ onReverseComplete: ()=>{
+            this.inactive();
+        }});
+
+        bounce_tl.to(this.moji, .3, {
+            transformOrigin: "50% 100%",
+            scaleX: 1.1,
+            scaleY: 0.8,
+            ease: "power1.inOut",
+            onComplete: () => {
+                bounce_tl.reverse();
+            }
+        })
     }
 
     jump(){
-        this.active('jumping', ()=>{
-            let second_bounce_tl = new TimelineMax({ paused: true, onReverseComplete: ()=>{
-                this.inactive('jumping');
-            }});
-
-            let jump_tl = new TimelineMax({ paused: true, onReverseComplete: ()=>{
-                second_bounce_tl.resume();
-            }});
-
-            let first_bounce_tl = new TimelineMax({ onReverseComplete: ()=>{
-                jump_tl.resume();
-            }});
-
-            second_bounce_tl.to(this.moji, .3, {
-                transformOrigin: "50% 100%",
-                scaleX: 1.1,
-                scaleY: 0.8,
-                ease: "power1.inOut",
-                onComplete: () => {
-                    second_bounce_tl.reverse();
-                }
-            })
-
-            first_bounce_tl.to(this.moji, .3, {
-                transformOrigin: "50% 100%",
-                scaleX: 1.05,
-                scaleY: 0.9,
-                ease: "power1.inOut",
-                onComplete: () => {
-                    first_bounce_tl.reverse();
-                }
-            });
-
-            jump_tl.to(this.moji, .2, {
-                y: -100,
-                onComplete: () => {
-                    jump_tl.reverse();
-                }
-            }, 0)
-            .to(this.shadow, .2, {
-                y: 100,
-                transformOrigin: "50% 50%",
-                scaleX: 1.075,
-                scaleY: 1.075,
-                opacity: .5,
-                onComplete: () => {
-                    jump_tl.reverse();
-                }
-            }, 0);
-        });
-    }
-
-    busy(){
-        return this.state.active.length != 0;
-    }
-
-    render(){
-        if(this.state.moji){
-            return html`<div style="display:inline;" innerHTML=${this.state.moji}></div>`
+        if (this.busy){
+            return;
         }
+        this.active();
+        let second_bounce_tl = new TimelineMax({ paused: true, onReverseComplete: ()=>{
+            this.inactive();
+        }});
+
+        let jump_tl = new TimelineMax({ paused: true, onReverseComplete: ()=>{
+            second_bounce_tl.resume();
+        }});
+
+        let first_bounce_tl = new TimelineMax({ onReverseComplete: ()=>{
+            jump_tl.resume();
+        }});
+
+        second_bounce_tl.to(this.moji, .3, {
+            transformOrigin: "50% 100%",
+            scaleX: 1.1,
+            scaleY: 0.8,
+            ease: "power1.inOut",
+            onComplete: () => {
+                second_bounce_tl.reverse();
+            }
+        })
+
+        first_bounce_tl.to(this.moji, .3, {
+            transformOrigin: "50% 100%",
+            scaleX: 1.05,
+            scaleY: 0.9,
+            ease: "power1.inOut",
+            onComplete: () => {
+                first_bounce_tl.reverse();
+            }
+        });
+
+        jump_tl.to(this.moji, .2, {
+            y: -100,
+            onComplete: () => {
+                jump_tl.reverse();
+            }
+        }, 0)
+        .to(this.shadow, .2, {
+            y: 100,
+            transformOrigin: "50% 50%",
+            scaleX: 1.075,
+            scaleY: 1.075,
+            opacity: .5,
+            onComplete: () => {
+                jump_tl.reverse();
+            }
+        }, 0);
+    }
+
+    active(){
+        this.busy = true;
+        this.ebb.pause();
+    }
+
+    inactive(){
+        this.busy = false;
+        this.ebb.resume();
     }
 }
-
-export { Moji }
