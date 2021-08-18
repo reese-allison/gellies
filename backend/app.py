d675b12087de4adbb0c061cdd1e0765d213f540c
gumphso <60444720+gumphso@users.noreply.github.com>
2021-08-18 18:51:31 -0400

    CORS Middleware

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 import minify_html
 import re
 
@@ -11,6 +12,19 @@ from backend.core.drops.drop_tables import get_moji, get_part_list
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="backend/static"), name="static")
 templates = Jinja2Templates(directory="backend/templates")
+origins = [
+    "http://127.0.0.1:8080",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class Gradient(BaseModel):
@@ -42,7 +56,6 @@ db_result = {
 @app.get("/moji-test/", response_class=HTMLResponse)
 async def moji_test(request: Request):
     svgs = get_moji('all')
-    # data =  minify_html.minify(templates.get_template("moji.html").render({"svgs": svgs}))
     data = templates.get_template("moji.html").render({"svgs": svgs})
     return Response(content=data, media_type="image/svg+xml")
 
@@ -50,12 +63,13 @@ async def moji_test(request: Request):
 async def moji_test(request: Request):
     svg_list = get_part_list()
     data = templates.get_template("selection.html").render({"svgs": svg_list})
-    return Response(content=data)
+    return templates.TemplateResponse("selection.html",{'request' : request, "svgs": svg_list})
+    return Response(content=data, media_type="application/xml")
 
 
-@app.get("/", response_class=HTMLResponse)
-async def index(request: Request):
-        return templates.TemplateResponse("index.html", {"request": request})
+#@app.get("/", response_class=HTMLResponse)
+#async def index(request: Request):
+#        return templates.TemplateResponse("index.html", {"request": request})
 
 #@app.post("/part-fetch", response_class=HTMLResponse)
 #async def part_fetch(appendage: Appendage):
