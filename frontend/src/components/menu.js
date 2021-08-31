209c6bb0b4f0416103057ca6a7978f2bc1f7ccdc
gumphso <60444720+gumphso@users.noreply.github.com>
2021-08-31 01:10:46 -0400

    Change Handling

--- frontend/src/components/menu.js
+++ frontend/src/components/menu.js
@@ -1,37 +1,36 @@
-import { useState, h, Fragment, Component, options } from 'preact';
+import { h, Fragment, Component, options } from 'preact';
 import { Select, InputLabel, MenuItem } from '@material-ui/core'
 import pageStyle from '../styles/pages'
 
-/*
-            // THIS IS DANGEROUS! TO REDUCE XSS ATTACKS, WE NEED TO SANITIZE this.state.content (See DomPurify)
-            <div className={pageclass.pageCenter}id='menu' dangerouslySetInnerHTML={{ __html: this.state.content }}></div>
-
-            {options.map((option) => (
-              <option value={option.value}>{option.label}</option>
-            ))}
- */
 class Menu extends Component{
     constructor(props){
         super(props);
         this.state = {
             gradient: 'random',
-            //g_html: null,
-            html: null,
+            g_html: '',
+            body: 'cute',
+            b_html: '',
+            eyes: 'cute',
+            e_html: '',
+            mouth: 'cute',
+            m_html: '',
             list: {},
         }
-        //console.log(this.fetch_html("/list")) 
     }
     
     /** @jsx h */
     /** @jsxFrag Fragment */
-    
-    fetch_html(url){
-        fetch(url)
+
+    fetch_html(url, tag){
+        const local_route = {
+            'gradient': "api/part/gradients/"+this.state.gradient,
+        }
+        fetch(local_route[url])
         .then(response => {
             return response.text();
         })
         .then(content => {
-            this.setState({html: content})
+            this.setState({[tag]: content})
         })
         .catch(function(error){
             console.log(error)
@@ -57,40 +56,23 @@ class Menu extends Component{
         if (prevState.gradient !== this.state.gradient){
             console.log(this.state.gradient)
             console.log("api/part/gradients/"+this.state.gradient)
-            this.fetch_html("api/part/gradients/"+this.state.gradient)
+            this.fetch_html('gradient', 'g_html')
             console.log(this.state.list)
-            //this.setState({g_html: this.state.html})
         }
     }
 
-    gradientChange = (event) =>{
-        this.setState({gradient: event.target.value})
-    }
-
-    Selections = () => {
-        const { list } = this.state
-        Object.keys(list).forEach(element =>
-            
-            parts = list.length > 0 && list.map((item, i) => {
-                return (
-                <MenuItem key={i} value={item.id}>{item.name}</MenuItem>
-                )
-            console.log(element)
-
-        )
+    handleChange = (event) =>{
+        const {value, name} = event.target
+        this.setState({[name]: value})
 
         /*
-        forEach(element => {
-            
-        });
-        let parts = list.length > 0
-		&& list.map((item, i) => {
-		return (
-			<MenuItem key={i} value={item.id}>{item.name}</MenuItem>
-		)
-	}, this);*/
+        console.log(event.name)
+        const {type, value, html_tag} = event.target
+        this.fetch_html('gradient', html_tag)
+        //this.setState({[html_tag]: value})
+        this.setState({[type]: event.target.value})
+        console.log(type)*/
     }
-
 
     render(props, state){
         const pageclass = pageStyle();
@@ -98,22 +80,21 @@ class Menu extends Component{
             <div className={pageclass.pageCenter} >
                 <svg style={{padding: "0% 40% 0% 40%"}} xmlns="http://www.w3.org/2000/svg" viewBox="-50 -50 600 600">
                 <defs>
-                    <g dangerouslySetInnerHTML={{__html: this.state.html}}> 
+                    <g id="gradients" dangerouslySetInnerHTML={this.state.g_html}> 
                         </g>
                     <filter id="shadow-blur" x="-100%" y="-100%" height="300%" width="300%">
                         <feGaussianBlur in="SourceGraphic" stdDeviation="20" />
                     </filter>
                 </defs>
-                <g id="bodies">
+                <g id="bodies" dangerouslySetInnerHTML={this.state.b_html}>
                 <ellipse id="moji-shadow" cx="245" cy="452" rx="219" ry="45" fill="black"  />
                 <path fill="url(#moji-menu-body-color)" style="stroke:black;stroke-width:.5px;stroke-opacity:1;" d="m114.5 214.3c65.4-39.5 183.5-45.7 264-2.1 60.1 36.8 96.7 107.2 77.6 172.2-9.9 44.1-48.5 76.5-92 85.2-70.2 18.2-173.3 15.9-240.2 4.8-42.5-8.2-82.2-38.8-93.6-89.8-21.9-64.7 16.5-132 84.1-170.2z" />
                 </g>
-                <g id="eyes"></g>
-                <g id="mouths"></g>  
+                <g id="eyes" dangerouslySetInnerHTML={this.state.e_html}></g>
+                <g id="mouths" dangerouslySetInnerHTML={this.state.m_html}></g>  
                 </svg>
-                <this.Selections/>
                 <InputLabel id="grad_label">Gradient</InputLabel>
-                <Select className={pageclass.pageMenu} labelId="grad_label" id="gradients" value={this.state.gradient} onChange = {this.gradientChange}>
+                <Select className={pageclass.pageMenu} labelId="grad_label" html_tag='g_html' name="gradient" value={this.state.gradient} onChange = {this.handleChange}>
                     <MenuItem value="random">random</MenuItem>
                     <MenuItem value="bone">bone</MenuItem>
                 </Select>
@@ -126,79 +107,3 @@ class Menu extends Component{
 
 export default Menu
 
-
-/*
-
-
-        if (prevProps.bodies !== this.state.bodies){
-            console.log("/path/bodies/"+this.state.bodies)
-            this.fetch_html("/path/bodies/"+this.state.bodies)
-        }
-        if (prevProps.eyes !== this.state.eyes){
-            console.log("/path/eyes/"+this.state.eyes)
-            this.fetch_html("/path/eyes/"+this.state.eyes)
-        }
-        if (prevProps.mouths !== this.state.mouths){
-            console.log("/path/mouths/"+this.state.mouths)
-            this.fetch_html("/path/mouths/"+this.state.mouths)
-        }
-
-
-
-
-    bodyChange = (event) =>{
-        this.setState({bodies: event.target.value})
-    }
-    eyeChange = (event) =>{
-        this.setState({eyes: event.target.value})
-    }
-    mouthChange = (event) =>{
-        this.setState({mouths: event.target.value})
-    }
-
-
-
-
-
-            <div className={pageclass.pageCenter}>
-                <svg style={{padding: "0% 40% 0% 40%"}}>
-                <defs>
-                    <radialGradient id="gradients"></radialGradient>
-                    <filter id="shadow-blur" x="-100%" y="-100%" height="300%" width="300%">
-                        <feGaussianBlur in="SourceGraphic" stdDeviation="20" />
-                    </filter>
-                </defs>
-                <g class="moji">
-                    <g id="bodies" class="moji">
-                        <ellipse id="moji-shadow" cx="245" cy="452" rx="219" ry="45" fill="rgba(0,0,0,.5)" filter="url(#shadow-blur)" />
-                        <path fill="url(#moji-body-color)" style="stroke:black;stroke-width:.5px;stroke-opacity:1;" d="m114.5 214.3c65.4-39.5 183.5-45.7 264-2.1 60.1 36.8 96.7 107.2 77.6 172.2-9.9 44.1-48.5 76.5-92 85.2-70.2 18.2-173.3 15.9-240.2 4.8-42.5-8.2-82.2-38.8-93.6-89.8-21.9-64.7 16.5-132 84.1-170.2z" />
-                    </g>
-                    <g id="eyes" class="moji"></g>
-                    <g id="mouths" class="moji"></g>  
-                </g>
-                </svg>
-                <InputLabel id="grad_label">Gradient</InputLabel>
-                <Select className={pageclass.pageMenu} labelId="grad_label" id="gradient" value={this.state.gradient} onChange = {this.gradientChange}>
-                    <MenuItem value="random">random</MenuItem>
-                    <MenuItem value="alien">alien</MenuItem>
-                </Select>
-                <InputLabel id="body_label">Body</InputLabel>
-                <Select className={pageclass.pageMenu} labelId="body_label" id="bodies" value={this.state.bodies} onChange = {this.bodyChange}>
-                    <MenuItem value="cute">cute</MenuItem>
-                    <MenuItem value="20">test2</MenuItem>
-                </Select>
-                <InputLabel id="eye_label">Eyes</InputLabel>
-                <Select className={pageclass.pageMenu} labelId="eye_label" id="eyes" value={this.state.eyes} onChange = {this.eyeChange}>
-                    <MenuItem value="cute">cute</MenuItem>
-                    <MenuItem value="20">test2</MenuItem>
-                </Select>
-                <InputLabel id="mouth_label">Mouth</InputLabel>
-                <Select className={pageclass.pageMenu} labelId="mouth_label" id="mouths" value={this.state.mouths} onChange = {this.mouthChange}>
-                    <MenuItem value="cute">cute</MenuItem>
-                    <MenuItem value="20">test2</MenuItem>
-                </Select>
-            </div>
-
-
-
-*/
