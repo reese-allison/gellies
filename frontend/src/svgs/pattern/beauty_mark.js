29e4361f37186316bc5efb3e89ea6b9874f11189
gumphso <60444720+gumphso@users.noreply.github.com>
2021-09-27 09:17:37 -0400

    Pattern convert to JS

---
+++ frontend/src/svgs/pattern/beauty_mark.js
@@ -0,0 +1,16 @@
+import { h, Fragment } from 'preact';
+import { memo } from 'preact/compat';
+
+
+/** @jsx h */
+/** @jsxFrag Fragment */
+
+function Beauty_mark(props){
+    return (
+        <g style={props.style}>
+            <circle fill="#000" cx="304.8" cy="394.6" r="3" />
+        </g>
+    )
+}
+
+export default memo(Beauty_mark);
--- frontend/src/svgs/pattern/index.js
+++ frontend/src/svgs/pattern/index.js
@@ -1,5 +1,18 @@
 import patchwork from './patchwork'
+import beauty_mark from './beauty_mark'
+import freckle from './freckle'
+import rosy from './rosy'
+import scar_left from './scar_left'
+import scar_right from './scar_right'
+import whisker from './whisker'
 
 export {
-    patchwork
+    patchwork,
+    beauty_mark,
+    freckle,
+    rosy,
+    scar_left,
+    scar_right,
+    whisker
+    
 }
